59751127ce8243e82c37374196bb7a3bbe09b543
Houssam El Hariri <hoel-har@f4r11s14.clusters.42paris.fr>
2026-01-09 16:43:24 +0100

    Function to sort from 1-5 finished, in a folder with the same name, norminette ok

---
+++ Sorting/sorting.c
@@ -0,0 +1,133 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sorting.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hoel-har <hoel-har@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/08 13:43:42 by hoel-har          #+#    #+#             */
+/*   Updated: 2026/01/09 16:40:28 by hoel-har         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+int	is_a_sorted(t_list *lst)
+{
+	t_list	*temp;
+
+	if (!lst)
+		return (0);
+	temp = lst->next;
+	if (lst->i > lst->next->i)
+		return (0);
+	while (temp != lst)
+	{
+		if ((temp->previous->i) > (temp->i))
+			return (0);
+		temp = temp->next;
+	}
+	return (1);
+}
+
+void	sort_2(t_list **lst)
+{
+	if (!lst)
+		return ;
+	sa(lst);
+}
+
+void	sort_3(t_list **lst)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	if (!lst || !(*lst) || is_a_sorted((*lst)))
+		return ;
+	a = (*lst)->i;
+	b = (*lst)->next->i;
+	c = (*lst)->previous->i;
+	if (c < a && a < b)
+		rra(lst);
+	else if (b < a && a < c)
+		sa(lst);
+	else if (a < c && c < b)
+	{
+		rra(lst);
+		sa(lst);
+	}
+	else if (b < c && c < a)
+		ra(lst);
+	else if (c < b && b < a)
+	{
+		sa(lst);
+		rra(lst);
+	}	
+}
+
+void	sort_4(t_list **lst_a, t_list **lst_b)
+{
+	if (!lst_a || !(*lst_a) || !lst_b)
+		return ;
+	if (is_a_sorted(((*lst_a))))
+		return ;
+	get_min_value(lst_a, lst_b);
+	sort_3(lst_a);
+	pa(lst_a, lst_b);
+}
+
+void	sort_5(t_list **lst_a, t_list **lst_b)
+{
+	if (!lst_a || !(*lst_a) || !lst_b)
+		return ;
+	if (is_a_sorted(((*lst_a))))
+		return ;
+	get_min_value(lst_a, lst_b);
+	get_max_value(lst_a, lst_b);
+	sort_3(lst_a);
+	pa(lst_a, lst_b);
+	ra(lst_a);
+	pa(lst_a, lst_b);
+}
+
+/* int	main(void)
+{
+	t_list *head = NULL;
+	t_list *tete = NULL;	
+	t_list *current;
+	// t_list *current1;
+
+	lst_add_back(&head, "9884");
+	lst_add_back(&head, "777");
+	lst_add_back(&head, "1");
+	lst_add_back(&head, "88");
+	lst_add_back(&head, "44");
+	sort_5(&head, &tete);
+	printf("-------    HEAD   -------------\n");
+	if (head)
+	{
+		current = head;
+		printf("head = %d\n", current->i);
+		current = head ->next;
+		while (current != head)
+		{
+			printf("head = %d\n", current->i);
+			current = current->next;
+		}
+	}
+	printf("-------    TETE   -------------\n");
+	
+	if (tete)
+	{
+		current = tete;
+		printf("tete = %d\n", current->i);
+		current = tete ->next;
+		while (current != tete)
+		{
+			printf("tete = %d\n", current->i);
+			current = current->next;
+		}
+	}
+	return 0;
+} */
---
+++ Sorting/sorting_utils.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sorting_utils.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hoel-har <hoel-har@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/09 16:38:25 by hoel-har          #+#    #+#             */
+/*   Updated: 2026/01/09 16:39:31 by hoel-har         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+void	get_min_value(t_list **lst, t_list **lst_b)
+{
+	t_list	*current;
+	t_list	*temp;
+	t_list	*min_node;
+
+	if (!(*lst) || !lst || !lst_b)
+		return ;
+	min_node = (*lst);
+	current = (*lst)->next;
+	while (current != (*lst))
+	{
+		if (min_node->i > current->i)
+			min_node = current;
+		current = current->next;
+	}
+	while (*lst != min_node)
+	{
+		ra(lst);
+	}
+	pb(lst, lst_b);
+}
+
+void	get_max_value(t_list **lst, t_list **lst_b)
+{
+	t_list	*current;
+	t_list	*temp;
+	t_list	*max_node;
+
+	if (!(*lst) || !lst || !lst_b)
+		return ;
+	max_node = (*lst);
+	current = (*lst)->next;
+	while (current != (*lst))
+	{
+		if (max_node->i < current->i)
+			max_node = current;
+		current = current->next;
+	}
+	while (*lst != max_node)
+		ra(lst);
+	pb(lst, lst_b);
+}
